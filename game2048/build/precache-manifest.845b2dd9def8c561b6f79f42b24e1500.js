f74f4a582b5b3c236f900b4f5800f325ffe63108
huntye1 <wm0548@qq.com>
2019-11-13 17:03:00 +0800

    add build

---
+++ game2048/build/precache-manifest.845b2dd9def8c561b6f79f42b24e1500.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b790d3497e1f562e08ebf78e043c229",
+    "url": "./index.html"
+  },
+  {
+    "revision": "d85ce76015dc0f5fc4db",
+    "url": "./static/css/main.e1232623.chunk.css"
+  },
+  {
+    "revision": "bc56e16504ab67eb5db9",
+    "url": "./static/js/2.2e4bc566.chunk.js"
+  },
+  {
+    "revision": "d85ce76015dc0f5fc4db",
+    "url": "./static/js/main.86eaf492.chunk.js"
+  },
+  {
+    "revision": "6ae9974420219f2d7d5d",
+    "url": "./static/js/runtime-main.f1307f59.js"
+  }
+]);
--- game2048/build/service-worker.js
+++ game2048/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.037eb40b4db818dc5ec9c59fd3eb09f3.js"
+  "./precache-manifest.845b2dd9def8c561b6f79f42b24e1500.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
